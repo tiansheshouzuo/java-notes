930c862518b308ad83823463f89d14fe8b5c2cb4
Administrator <Administrator@9ZSXIKUS3BIPJMJ>
2019-01-15 23:21:10 +0800

    first commit

---
+++ src/j2se/Array.java
@@ -0,0 +1,8 @@
+package j2se;
+public class Array {
+public static void main(String[] args) {
+int a[]=new int[10];
+int b=a.length; //得到数组的长度
+System.out.println("数组a的长度为："+b);
+}
+}
---
+++ src/j2se/Test.java
@@ -0,0 +1,8 @@
+package j2se;
+
+public class Test{
+	 public static void main(String args[]){
+		 int a[][]={{1},{3,2},{6,7,8},{9}};
+		 System.out.println(a[2][1]);
+	 }
+	}
